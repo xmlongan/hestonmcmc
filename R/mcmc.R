0d9828fa5c932b9d14bbf85c08683963bd9ed739
Yanfeng <xmlongan@users.noreply.github.com>
2023-08-14 20:01:58 +0800

    Update mcmc.R
    offer users the option to provide parameter initial values

--- R/mcmc.R
+++ R/mcmc.R
@@ -27,6 +27,7 @@
 #' * \eqn{v_N}: [rvN()]
 #'
 #' @param y vector of returns \[\eqn{y_1, ..., y_N}\].
+#' @param ini_par vector of parameter initial values.
 #' @param g number of warm-up iteration, defaults to 5,000.
 #' @param G number of total iteration, defaults to 10,000.
 #' @param h time unit, defaults to 1.
@@ -41,11 +42,12 @@
 #'
 #' @examples
 #' y = rep(0.125,20)
-#' mcmc(y)
-mcmc <- function(y,g=5000,G=10000,h=1,echo=FALSE) {
+#' ini_par=c(0.0625,0.05,0.125,0.05,-0.35)
+#' mcmc(y, ini_par)
+mcmc <- function(y, ini_par, g=5000, G=10000, h=1, echo=FALSE) {
   # g: warm-up samples
   # G: total samples
-  init_values = initialize_values(y, parameters=c(0,0.01,0.1,0.01,0), h=1)
+  init_values = initialize_values(y, ini_par=ini_par, h=1)
   #
   mu      = init_values$parameters[1]
   k       = init_values$parameters[2]
