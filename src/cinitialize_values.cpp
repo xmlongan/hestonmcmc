ad512df44b011e53aa74f671116c49c6f7c27bed
Yanfeng <xmlongan@users.noreply.github.com>
2023-08-14 19:57:10 +0800

    Update cinitialize_values.cpp
    offer users the option to provide the parameter initial values

--- src/cinitialize_values.cpp
+++ src/cinitialize_values.cpp
@@ -5,17 +5,19 @@ using namespace Rcpp;
 
 //' @rdname initialize_values
 // [[Rcpp::export]]
-NumericVector cinitialize_values(NumericVector y, NumericVector parameters,
+NumericVector cinitialize_values(NumericVector y, NumericVector ini_par,
                                  double h) {
   double std_eps_y, mu_v, sd_v;
   // initialize parameter values
   // mu=0, k=0.01, theta=0.1, sigma_v=0.01, rho=0
-  parameters[0] = 0;
-  parameters[1] = 0.01;
-  parameters[2] = 0.1;
-  parameters[3] = 0.01;
-  parameters[4] = 0;
-  double mu=0, k=0.01, theta=0.1, sigma_v=0.01, rho=0;
+  //parameters[0] = 0;
+  //parameters[1] = 0.01;
+  //parameters[2] = 0.1;
+  //parameters[3] = 0.01;
+  //parameters[4] = 0;
+  //double mu=0, k=0.01, theta=0.1, sigma_v=0.01, rho=0;
+  double mu=ini_par[0], k=ini_par[1], theta=ini_par[2];
+  double sigma_v=ini_par[3], rho=ini_par[4];
   //
   int N = y.size();
   NumericVector v (N+1);
