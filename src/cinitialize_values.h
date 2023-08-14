396cacbfc8b4a9e61358b013a019ef1c6d7e63d2
Yanfeng <xmlongan@users.noreply.github.com>
2023-08-14 20:06:16 +0800

    Update cinitialize_values.h

--- src/cinitialize_values.h
+++ src/cinitialize_values.h
@@ -1,5 +1,5 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
-NumericVector cinitialize_values(NumericVector y, NumericVector parameters,
+NumericVector cinitialize_values(NumericVector y, NumericVector ini_par,
                                  double h);
